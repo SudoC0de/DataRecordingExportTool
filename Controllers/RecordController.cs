4039039f218c8f406a82b390b5ee9a869f5e02b9
Colin <functioncf@gmail.com>
2023-02-28 22:47:41 -0500

    Added ability to dynamically create new UI elements on the Create Table page for column creation. To-Do: Add ability to scroll left and right on page if too many column definition fields are present.

--- Controllers/RecordController.cs
+++ Controllers/RecordController.cs
@@ -24,8 +24,6 @@ public class RecordController : Controller
     [ValidateAntiForgeryToken]
     public IActionResult CreateResultsTable()
     {
-
-
         return View("CreateTable");
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function AddNewColumn() {
+    var td = document.createElement('td');
+
+    td.innerHTML = NewColumn();
+    document.getElementById("ColumnDefinitions")
+            .appendChild(td);
+}
+
+function NewColumn() {
+    return '<div class="form-group">' +
+                '<input class="form-control" />' +
+                '<button class="btn btn-primary">Delete</button>' +
+           '</div>';
+}
+
+function RemoveColumnDefinition(td) {
+    document.getElementById("ColumnDefinitions")
+            .removeChild(td.parentNode);
+}
